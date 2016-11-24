17d519f0e72704694d31cdd36fa25e770b0ca676
Bjorn Ansems <bjorn.ansems@inmation.com>
2016-11-24 17:11:18 +0100

    Added table-extension test: test_ifind3

--- tests/inmation/table-extension_tests.lua
+++ tests/inmation/table-extension_tests.lua
@@ -18,10 +18,20 @@ tests = {
         assert(result == 'world' and idx == 2, string.format("Proper item not found: %s, %s", result, idx))
     end,
 
+    test_ifind3 = function(self)
+        local tbl = { }
+        local item = { Name = 'Test'}
+        local result, idx = table.ifind(tbl, function(storedItem) 
+            return item.Name == storedItem.Name
+        end)
+        assert(result == nil and idx == nil, string.format("Item should not be found: %s, %s", result, idx))
+    end,
+
     execute = function(self)
         print('Begin table-extension_tests')
         self:test_ifind1()
         self:test_ifind2()
+        self:test_ifind3()
         print('End table-extension_tests')
     end
 }
