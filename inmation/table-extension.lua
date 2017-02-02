2114daff85bfaf39d84829acbc5509acae3c4577
Alexandr Sapunji <alexandr.sapunji@inmation.com>
2017-02-02 10:48:55 +0100

    Added contains, concat and dictlen functions

--- inmation/table-extension.lua
+++ inmation/table-extension.lua
@@ -76,3 +76,28 @@ function table.ifind(tbl, predicate)
         end
     end
 end
+
+function table.contains(tbl, elem)
+	if type(tbl) ~= 'table' then return nil end
+	for _,val in pairs(tbl) do
+		if val == item then return true end
+	end
+	return false
+end
+
+function table.concat(t1, t2)
+	if type(t1) == 'table' and type(t2) == 'table' then	
+		for i=1, #t2 do	t1[#t1+1] = t2[i] end
+		return t1
+	else
+		return nil, "Both parameters must be of type table"
+	end
+end
+
+function table.dictlen(dict)
+	if type(dict) ~= 'table' then return nil end
+	local count = 0
+	for _,_ in pairs(dict) do count = count + 1	end
+	
+	return count
+end
