bf0040853473e9e55d6b8396c2ad588a6122e539
Marc van de Langenberg <marc.vandelangenberg@inmation.com>
2017-03-20 10:05:16 +0100

    Examples how to add functions to the inmation mock.

--- tests/mock/inmation_tests.lua
+++ tests/mock/inmation_tests.lua
@@ -18,11 +18,48 @@ tests = {
        -- assert(dateTime ~= expectedResult, string.format("test_gettimebynumber failed; Expected '%s' got '%s'", expectedResult, dateTime))
     end,
 
+    test_override = function(self)
+        inmation.setOverride('createobject', function (parentPath, modelClass, type)
+            return { 
+                id = 1,
+                parentPath = parentPath,
+                modelClass = modelClass,
+                type = type
+            }
+        end)
+
+        local obj = inmation.createobject('/helloworld/', 'Folder', 'OBJ')
+        assert(obj.id == 1, "Should recieve object with id = 1")
+    end,
+
+    test_get_override = function(self)
+        inmation.setOverride('getobject', function (objPath)
+            return { 
+                id = 999
+            }
+        end)
+
+        local obj = inmation.getobject('/helloworld/')
+        assert(obj.id == 999, "Should recieve object with id = 999")
+    end,
+
+    test_getvalue_override = function(self)
+        inmation.setOverride('getvalue', function (propPath)
+            return 1234
+        end)
+
+        local value = inmation.getvalue('/helloworld/')
+        assert(value == 1234, "Should receive value 1234")
+    end,
+
     execute = function(self)
         print("Begin inmation_tests")
         self:test_now()
         self:test_currenttime()
         self:test_gettimebynumber()
+        self:test_override()
+        self:test_get_override()
+        self:test_getvalue_override()
         print("End inmation_tests")
     end
 }
